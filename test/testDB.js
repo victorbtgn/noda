6fb9f76bdba79d76e93265e04531e254c223b5d5
Victor Batigin <bixs85@gmail.com>
2020-10-07 22:43:11 +0300

    rework middleware test

--- test/testDB.js
+++ test/testDB.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { func } = require("joi");
 const { connect } = require("mongoose");
 const sinon = require("sinon");
 const { mockReq, mockRes } = require("sinon-express-mock");
@@ -14,50 +15,59 @@ it("Connection mongo test with valid URI", (done) => {
   });
 });
 
-const validToken = {
-  get: () =>
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNzRlYmQ5MGMwOGIzMzU5ODlkNzVjOCIsImlhdCI6MTYwMTY1NDAyMH0.bSaMyhIWYzjnx8aVIIDh8QwGLxY-UhmR4pjUT_nmang",
+const argForMiddleware = () => {
+  let req = mockReq();
+  let res = mockRes({ params: {} });
+  let next = sinon.stub();
+  res.status = function (params) {
+    this.params.status = params;
+    return this;
+  };
+  res.json = function (params) {
+    this.params.json = params;
+    return this;
+  };
+
+  return [req, res, next];
 };
+
+const validToken = () =>
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNzRiNTE1ZTQ4ODRkMmM1NDcwMzA3NyIsImlhdCI6MTYwMTQ4NDEyOX0.YK-fzBeDVHckGo7NzuQTRxD1_9A2qjBViZqzabGqTlk";
 
 it("Check auth middleware with valid token", (done) => {
-  const fakeReq = mockReq(validToken);
-  const fakeRes = mockRes();
+  const [req, res, next] = argForMiddleware();
+  req.get = validToken;
 
-  checkAuthTokenMiddleware(fakeReq, fakeRes, (err, req) => {
-    if (err) return done(err);
-    done();
+  checkAuthTokenMiddleware(req, res, next).then(() => {
+    if (req.user) return done();
+    done(new Error("No message exists"));
   });
 });
 
-const invalidToken = {
-  get: () =>
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNzRlYmQ5MGMwOGIzMzU5ODlkNzVjOCIsImlhdCI6MTYwMTY1NDAyMH0.bSaMyhIWYzjnx8aVIIDh8QwGLxY-UhmR4pjUT_nmanG",
-};
+const invalidToken = () =>
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNzRlYmQ5MGMwOGIzMzU5ODlkNzVjOCIsImlhdCI6MTYwMTY1NDAyMH0.bSaMyhIWYzjnx8aVIIDh8QwGLxY-UhmR4pjUT_nmanG";
 
 it("Check auth middleware with invalid token", (done) => {
-  const fakeReq = mockReq(invalidToken);
-  const fakeRes = mockRes();
+  const [req, res, next] = argForMiddleware();
+  req.get = invalidToken;
 
-  checkAuthTokenMiddleware(fakeReq, fakeRes, (err) => {
-    if (err) return done();
-    done(new Error("must be invalid token"));
+  checkAuthTokenMiddleware(req, res, next).then(() => {
+    if (res.params.status === 401) return done();
+    done(new Error("No message exists"));
   });
 });
 
-const nonToken = {
-  get: () => "",
-};
+const nonToken = () => "";
 
-// it("Check auth middleware without token", (done) => {
-//   const fakeReq = mockReq(nonToken);
-//   const fakeRes = mockRes();
+it("Check auth middleware without token", (done) => {
+  const [req, res, next] = argForMiddleware();
+  req.get = nonToken;
 
-//   checkAuthTokenMiddleware(fakeReq, fakeRes, (err) => {
-//     console.log("ERR: ", err);
-//     if (err) return done();
-//     done(new Error("must be empty token"));
-//   });
-// });
+  checkAuthTokenMiddleware(req, res, next).then(() => {
+    if (res.params.status === 401) return done();
+    done(new Error("No message exists"));
+  });
+});
 
 // ======================================
 // task 2 supertest
