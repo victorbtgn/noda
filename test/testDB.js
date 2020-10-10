a0f550f0bffc0d086a4c12e338dc3e111fa0baf2
Victor Batigin <bixs85@gmail.com>
2020-10-11 00:59:51 +0300

    added 2nd test

--- test/testDB.js
+++ test/testDB.js
@@ -68,3 +68,35 @@ describe('Unit test for auth middleware', () => {
     if(res.params.status !== 401) throw new Error('Some error');
   });
 })
+
+describe('Check POST upload avatar', function () {
+  const app = express();
+  app.use(express.urlencoded({ extended: false }))
+  app.use("/auth", authRouter);
+
+  it('Check middleware with invalid token',  (done) => {
+    const invalidToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmN2Y0MTkxNWVmZDgxMjE0YzRlNjk5YyIsImlhdCI6MTYwMjE3NTQxOH0.9Dz4kOJ6hLT8R8gQGhNEfvSk52B_AQhajO1-q9G5W38===?:sdf';
+    request(app)
+      .post('/auth/users/avatars')
+      .set('Authorization', invalidToken)
+      .expect(401)
+      .end((err, res) => {
+        if(err) return done(new Error('Failed test'))
+        res.status.should.equal(401)
+        done()
+      })
+  });
+
+  it('Check status and body on update avatar', function (done) {
+    const validToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmN2Y0MTkxNWVmZDgxMjE0YzRlNjk5YyIsImlhdCI6MTYwMjE3NTQxOH0.9Dz4kOJ6hLT8R8gQGhNEfvSk52B_AQhajO1-q9G5W38';
+
+    request(app)
+      .post('/auth/users/avatars')
+      .set('Authorization', validToken)
+      .field('name', 'default avatar')
+      .attach('avatar', 'test/avatar.jpg')
+      .expect(200, {        
+        avatarURL: 'http://localhost:8000/images/5f7f41915efd81214c4e699c.jpeg',
+      }, done)
+  })
+});
